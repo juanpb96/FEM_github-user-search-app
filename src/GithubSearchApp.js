c53fc2411f2ea89168978fbb2d52284851b684bb
MasterDev <juanpb.96@gmail.com>
2022-04-30 15:59:41 -0500

    Update structure and add style classes

--- src/GithubSearchApp.js
+++ src/GithubSearchApp.js
@@ -1,21 +1,49 @@
+import { useEffect, useRef, useState } from 'react';
 import { ProfileCard } from './components/ProfileCard/ProfileCard';
 import { SearchBar } from './components/SearchBar/SearchBar';
 import './styles/main.scss';
 
+const bodyTag = document.querySelector('body');
+
 export const GithubSearchApp = () => {
+ 
+    const [theme, setTheme] = useState('light');
+    const refTheme = useRef(null);
+
+    useEffect(() => {
+        const lastTheme = refTheme.current.innerText.toLowerCase();
+        bodyTag.classList.add(`theme-${theme}`);
+        bodyTag.classList.remove(`theme-${lastTheme}`);
+    }, [theme]);
+
+    const handleThemeToggle = () => {
+        setTheme( theme === 'light' ? 'dark' : 'light' );
+    };
+
     return (
-        <>
-            <header>
-                <h1>devfinder</h1>
-                <div className='theme-toggle'>
-                    <span>Dark</span>
-                    <img src='./assets/icon-moon.svg' alt='' />
-                </div>
+        <div className='app-container'>
+            <header className='[ app-header ] [ flex-flow ]'>
+                <h1 className='[ title-color fw-700 ]'>devfinder</h1>
+                <button 
+                    type='button' 
+                    className='[ theme-toggle ] [ flex-flow ]'
+                    onClick={ handleThemeToggle }
+                >
+                    <span 
+                        className='[ theme-toggle__name ] [ text-color-secondary fs-small fw-700 uppercase ]'
+                        ref={ refTheme }
+                    >{ theme === 'light' ? 'dark' : 'light' }</span>
+                    {
+                        theme === 'light'
+                        ? <img className='theme-toggle__img' src='./assets/icon-moon.svg' alt='' />
+                        : <img className='theme-toggle__img' src='./assets/icon-sun.svg' alt='' />
+                    }
+                </button>
             </header>
-            <main>
+            <main className='[ app-main ]'>
                 <SearchBar />
                 <ProfileCard />
             </main>
-        </>
+        </div>
     );
 };
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,44 @@
+import { useState } from "react";
 
 
 export const SearchBar = () => {
 
+    const [username, setUsername] = useState('');
+
+    const handleInputChange = ({target}) => {
+        setUsername(target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <form className='search-bar'>
-            <img src='./assets/icon-search.svg' alt='' />
+        <form 
+            className='[ search-bar ] [ flex-flow ] [ bg-secondary card-border mb-16 ]'
+            onSubmit={ handleSubmit }
+        >
+            <img 
+                className='icon'
+                src='./assets/icon-search.svg' 
+                alt='' 
+            />
             <input 
                 type='text'
                 name='username'
-                value=''
+                value={ username }
+                onChange={ handleInputChange }
                 placeholder='Search GitHub username…'
+                className='[ search-bar__input ] [ text-color-secondary fw-400 ]'
             />
-            <p className='search-bar__error'>No results</p>
-            <button type='submit'>Search</button>
+            <p 
+                className='[ search-bar__error ] [ text-error-color fw-700 ]'
+            >No results</p>
+            <button 
+                type='submit'
+                className='[ btn ] [ text-color-white fw-700 br-10 ml-auto ]'
+                datatype='blue'
+            >Search</button>
         </form>
     );
 };
