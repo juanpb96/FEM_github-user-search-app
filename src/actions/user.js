b16f3a878ba5bf952c0a46751ad4877b4a7e8122
MasterDev <juanpb.96@gmail.com>
2022-05-14 21:21:34 -0500

    Updates

--- src/actions/user.js
+++
@@ -1,6 +0,0 @@
-import { types } from "../types/types";
-
-export const setUserData = (userData) => ({
-    type: types.setUserData,
-    payload: userData
-});
--- src/types/types.js
+++
@@ -1,3 +0,0 @@
-export const types = {
-    setUserData: '[user] Set data'
-};
