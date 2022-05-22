a6a95dae72ba71106e319be152767324468d4787
MasterDev <juanpb.96@gmail.com>
2022-05-21 20:43:29 -0500

    Fix media query value

--- src/helpers/getTheme.js
+++ src/helpers/getTheme.js
@@ -1,6 +1,6 @@
 export const getTheme = () => {
     const localTheme = localStorage.getItem('theme');
-    const isSystemLightTheme = window.matchMedia("(prefers-color-scheme: ligth)").matches;
+    const isSystemLightTheme = window.matchMedia("(prefers-color-scheme: light)").matches;
     const systemTheme = isSystemLightTheme ? 'light' : 'dark';
 
     const userTheme = localTheme ? localTheme : systemTheme;
